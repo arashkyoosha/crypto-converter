827a4602c9951e6327682c063ab35474e10d3edd
Arashk <arashkyoosha@gmail.com>
2022-04-17 14:50:46 -0400

    made adjustments to html and js files

--- assets/js/convert.js
+++ assets/js/convert.js
@@ -1,17 +1,20 @@
 var mainCon = document.getElementById("container");
 
-const getBtcData = async () => {
-  fetch("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,XRP,SHIB&tsyms=USD")
+const getCryptoData = async () => {
+  fetch("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,XRP,SHIB&tsyms=USD"
+  )
     .then((response) => response.json())
     .then((data) => {
       for (const key in data) {
         console.log(key, data[key].USD);
         var tag = document.createElement("p");
+        tag.setAttribute("id", "p");
         tag.innerHTML = `Current ${key} price: $` + data[key].USD;
         mainCon.appendChild(tag);
         var amount = document.getElementById("usd").value;
         var convertedPrice = amount / data[key].USD;
         tag = document.createElement("p");
+        tag.setAttribute("id", "p");
         tag.innerHTML = "You can buy " + convertedPrice + " share of " + key;
         mainCon.appendChild(tag);
         console.log(convertedPrice);
@@ -20,5 +23,10 @@ const getBtcData = async () => {
 };
 
 function takeInput() {
-  getBtcData();
+  getCryptoData();
+}
+
+function clearAll() {
+  $("p").empty();
+  document.getElementById("usd").value = "";
 }
